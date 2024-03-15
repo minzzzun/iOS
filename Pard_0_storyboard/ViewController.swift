f9c7736fdade76a8aad2993d7b4104d23592005f
minzzzun <22000078@handong.ac.kr>
2024-03-15 19:17:11 +0900

    Initial Commit

---
+++ Pard_0_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pard_0_storyboard
+//
+//  Created by 김민준 on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
